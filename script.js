195ab5b3969424e0cbd3e41eca27c8ac35bd018b
Alec_Zam <47293857+aloc123@users.noreply.github.com>
2025-10-13 23:29:55 +0800

    Add the function to take the user's input

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function getComputerChoice() {
     //math.random generates 0 to 1, if I multiply to 3, I generate 0 to 3, but if i get its floor i get 0 to 2
     return Math.floor(Math.random() * 3);
 }
-
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    //get the user input using prompt and return it
+    return prompt("Rock, Paper, or Scissors?");
+}
+console.log(`You chose ${getHumanChoice()}!`);
